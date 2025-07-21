8e3ba735bf71744d10ca0a443d2b092807300e34
Diego Ramirez <diego.ramirez2d03@gmail.com>
2025-07-21 11:31:20 -0600

    Project Initialization and Tailwind Configuration

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-Purple-600 text-2xl font-Ubuntu-Regular text-center">Multi Step Form</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
